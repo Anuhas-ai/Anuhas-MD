e3c2417ec629fa66b09350fdaad429ec6bc07b28
Anuhas-ai <gueyaue@gmail.com>
2025-06-26 18:35:17 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=e0hEABjZ#M-PpKU5MJ6qHxr6b_Sl3-yHyImEW1J6adb-asveivJM' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
